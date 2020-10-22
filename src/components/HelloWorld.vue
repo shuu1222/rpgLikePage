50772b16abb9be0e8e164aa81e252f6b506144c6
shuhei wada <shuu1222@gmail.com>
2020-10-22 23:52:16 +0900

    メインダイアログコンポーネントの追加 王様と会話の追加

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,26 +7,30 @@
           v-for="(xItems, xIndex) in moveArea[index]"
           :key="xIndex"
         >
-          <img
-            :src="
-              moveArea[index][xIndex] === 1
-                ? require(`../assets/images/${playerDirection}.png`)
-                : ''
-            "
-            alt=""
-          />
+          <img :src="imageSources(index, xIndex)" alt="" />
           <!-- 座標テスト -->
           <!-- {{moveArea[index][xIndex]}} -->
         </div>
         <br />
       </div>
     </div>
+    <AppMainDialog
+      :positionY="positionY"
+      :positionX="positionX"
+      @setMoveRestriction="setMoveRestriction()"
+      @resetMoveRestriction="resetMoveRestriction()"
+    ></AppMainDialog>
   </v-container>
 </template>
 <script lang="ts">
 import Vue from "vue";
+import AppMainDialog from "./mainDialog.vue";
+
 export default Vue.extend({
   name: "HelloWorld",
+  components: {
+    AppMainDialog
+  },
   data: () => ({
     moveArea: [
       [0, 0, 3, 3, 0, 0, 0, 0, 3, 0, 0, 0, 3, 3, 3, 3, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0],
@@ -37,7 +41,7 @@ export default Vue.extend({
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 0],
@@ -49,7 +53,9 @@ export default Vue.extend({
     positionY: 14,
     positionX: 12,
     direction: "front1", //front,up,right,left
-    moveAnimation: false
+    moveAnimation: false,
+    movementRestriction: false,
+    isTalking: false
   }),
   created() {
     this.moveArea[this.positionY][this.positionX] = 1;
@@ -60,6 +66,7 @@ export default Vue.extend({
     this.$once("hook:beforeDestroy", () => {
       document.removeEventListener("keydown", eventHandler);
     });
+    this.resetRestriction();
   },
   computed: {
     playerDirection(): string {
@@ -67,31 +74,59 @@ export default Vue.extend({
     }
   },
   methods: {
+    imageSources(index:number, xIndex:number) {
+      return this.moveArea[index][xIndex] === 1
+        ? require(`../assets/images/${this.playerDirection}.png`)
+        : this.moveArea[index][xIndex] === 6
+        ? require(`../assets/images/king.png`)
+        : "";
+    },
     // キーダウンイベントを感知した際の処理
     pressedKey(e: KeyboardEvent): void {
+      if (this.movementRestriction || this.isTalking) return;
+      // 方向キーの↑が押下された際の処理
       if (e.key === "ArrowUp" && this.positionY != 0) {
-        this.moveAnimation ? this.direction = "up2" : this.direction = "up3"
-        this.moveAnimation = !this.moveAnimation
+        this.moveAnimation
+          ? (this.direction = "up2")
+          : (this.direction = "up3");
+        this.moveAnimation = !this.moveAnimation;
         this.pressedKeyUp();
+        // 方向キーの↓が押下された際の処理
       } else if (e.key === "ArrowDown" && this.positionY != 15) {
-        this.moveAnimation ? this.direction = "front2" : this.direction = "front3"
-        this.moveAnimation = !this.moveAnimation
+        this.moveAnimation
+          ? (this.direction = "front2")
+          : (this.direction = "front3");
+        this.moveAnimation = !this.moveAnimation;
         this.pressedKeyDown();
+        // 方向キーの→が押下された際の処理
       } else if (e.key === "ArrowRight" && this.positionX != 25) {
-        this.moveAnimation ? this.direction = "right2" : this.direction = "right3"
-        this.moveAnimation = !this.moveAnimation
+        this.moveAnimation
+          ? (this.direction = "right2")
+          : (this.direction = "right3");
+        this.moveAnimation = !this.moveAnimation;
         this.pressedKeyRight();
+        // 方向キーの←が押下された際の処理
       } else if (e.key === "ArrowLeft" && this.positionX != 0) {
-        this.moveAnimation ? this.direction = "left2" : this.direction = "left3"
-        this.moveAnimation = !this.moveAnimation
+        this.moveAnimation
+          ? (this.direction = "left2")
+          : (this.direction = "left3");
+        this.moveAnimation = !this.moveAnimation;
         this.pressedKeyLeft();
       }
+      this.movementRestriction = true;
+    },
+    // 一定時間おきに移動制限をリセットする
+    resetRestriction(): void {
+      setTimeout(() => {
+        this.movementRestriction = false;
+        this.resetRestriction();
+      }, 150);
     },
     // 方向キーの↑が押下された際の処理
     async pressedKeyUp(): Promise<void> {
       const y = this.positionY - 1;
       const upperPosition = this.moveArea[y][this.positionX];
-      if (upperPosition === 3) {
+      if (upperPosition >= 3) {
         return;
       } else {
         await this.moveArea[this.positionY].splice(this.positionX, 1, 0);
@@ -103,7 +138,7 @@ export default Vue.extend({
     async pressedKeyDown(): Promise<void> {
       const y = this.positionY + 1;
       const bottomPosition = this.moveArea[y][this.positionX];
-      if (bottomPosition === 3) {
+      if (bottomPosition >= 3) {
         return;
       } else {
         await this.moveArea[this.positionY].splice(this.positionX, 1, 0);
@@ -115,7 +150,7 @@ export default Vue.extend({
     async pressedKeyLeft(): Promise<void> {
       const x = this.positionX - 1;
       const leftPosition = this.moveArea[this.positionY][x];
-      if (leftPosition === 3) {
+      if (leftPosition >= 3) {
         return;
       } else {
         await this.moveArea[this.positionY].splice(this.positionX, 1, 0);
@@ -127,13 +162,21 @@ export default Vue.extend({
     async pressedKeyRight(): Promise<void> {
       const x = this.positionX + 1;
       const rightPosition = this.moveArea[this.positionY][x];
-      if (rightPosition === 3) {
+      if (rightPosition >= 3) {
         return;
       } else {
         await this.moveArea[this.positionY].splice(this.positionX, 1, 0);
         this.positionX += 1;
         this.moveArea[this.positionY].splice(this.positionX, 1, 1);
       }
+    },
+    // 会話ダイアログの表示が完了するまで移動制限をオンにする
+    setMoveRestriction(): void {
+      this.isTalking = true;
+    },
+    // 移動制限を解除する
+    resetMoveRestriction(): void {
+      this.isTalking = false;
     }
   }
 });
@@ -142,8 +185,9 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .move-area {
   &-wrapper {
-    background-size:1050px auto;
+    background-size: 1050px auto;
     width: 1050px;
+    height: 615px;
     position: relative;
     background-image: url("../assets/mapdata.png");
     text-align: center;
@@ -159,7 +203,7 @@ export default Vue.extend({
   &-x {
     display: inline-block;
     width: 40px;
-    height: 40px;
+    height: 39px;
     z-index: 1;
   }
 }
